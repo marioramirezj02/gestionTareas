a386bd2783da0add8b9fbcc099709fa93f64884b
marioramirezj02 <marioramirezj02@gmail.com>
2025-12-19 19:07:14 +0100

    Pruebas Factory (TODO)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory(1)->create();
 
-
+ 
         Proyectos::create(['nombre' => 'Proyecto 1', 'id_usuario' => User::first()->id,'descripcion' => 'texto ejemplo primer proyecto']);        
         Proyectos::create(['nombre' => 'Proyecto 2', 'id_usuario' => User::first()->id,'descripcion' => 'texto ejemplo segundo proyecto']);
         Proyectos::create(['nombre' => 'Proyecto 3', 'id_usuario' => User::first()->id,'descripcion' => 'texto ejemplo tercer proyecto']);
@@ -36,9 +36,16 @@ class DatabaseSeeder extends Seeder
         Tareas::create(['titulo' => 'Tarea 1 de Proyecto 2', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(1)->take(1)->first()->id, 'completado' => false]);
         Tareas::create(['titulo' => 'Tarea 2 de Proyecto 2', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(1)->take(1)->first()->id, 'completado' => false]);
         Tareas::create(['titulo' => 'Tarea 3 de Proyecto 2', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(1)->take(1)->first()->id, 'completado' => false]);
-        
+
         Tareas::create(['titulo' => 'Tarea 1 de Proyecto 3', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(2)->take(1)->first()->id, 'completado' => false]);
         Tareas::create(['titulo' => 'Tarea 2 de Proyecto 3', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(2)->take(1)->first()->id, 'completado' => false]);
         Tareas::create(['titulo' => 'Tarea 3 de Proyecto 3', 'id_proyecto' => Proyectos::orderBy('id','asc')->skip(2)->take(1)->first()->id, 'completado' => false]);
+
+
+        //  Fallos de foreign keys en los factory TODO 
+        
+        //Proyectos::factory(3)->create();
+        //Tareas::factory(9)->create();
+
     }
 }
